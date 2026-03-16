ca810645d8ee918076f7d9a8f1f1bedb1b762b24
Rakshith Acharya <rakshixh@gmail.com>
2026-03-16 20:45:18 +0530

    removed unwanted code

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import Header from "../components/Header";
 import Portfolio from "../components/Portfolio";
 import Footer from "../components/Footer";
