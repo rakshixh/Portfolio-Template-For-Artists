a81a996f4ce52b5fc11882067c50c6ac543e9955
Rakshith Acharya <rakshixh@gmail.com>
2026-08-03 23:43:32 +0530

    added vite configs

---
+++ src/App.jsx
@@ -0,0 +1,40 @@
+import React, { Suspense, lazy, useEffect } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "./index.css";
+import { Toaster } from "react-hot-toast";
+import Loader from "./components/Loader";
+
+const Home = lazy(() => import("./pages/Home"));
+const About = lazy(() => import("./pages/About"));
+const NotFound = lazy(() => import("./pages/NotFound"));
+
+const App = () => {
+  useEffect(() => {
+    const handleContextMenu = (event) => {
+      event.preventDefault();
+    };
+
+    document.addEventListener("contextmenu", handleContextMenu);
+    // Cleanup the event listener on component unmount
+    return () => {
+      document.removeEventListener("contextmenu", handleContextMenu);
+    };
+  }, []);
+
+  return (
+    <div className="App">
+      <Router>
+        <Suspense fallback={<Loader />}>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </Suspense>
+      </Router>
+      <Toaster />
+    </div>
+  );
+};
+
+export default App;
